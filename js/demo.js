e958cfba7dd0a4246ac95c94b95163bf151bb3ce
narasimha kbnd <narasimha kbnd>
2016-10-19 08:06:46 -0700

    Added code

---
+++ js/demo.js
@@ -0,0 +1,30 @@
+/*var count = 10; // integer datatype
+var pi = 3.14; //
+var name = "Kiran";
+var users = []; //array
+var iAgree = false; //boolean.*/
+
+//function syntax
+/*function <functionname>()
+
+{
+    
+}*/
+/*function declaration.
+ functioin parameters a,b
+ */
+
+function add(a,b) {
+ var result = a+b;
+    alert(result);
+}
+
+
+
+
+
+//function execution
+add(1,2);
+add("kiran","paturi");
+
+add([1,2,3],[4,5,6]);
