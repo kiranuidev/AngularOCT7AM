999cd77ace047eaa51f077cc47c618a2bf0f1a18
narasimha kbnd <narasimha kbnd>
2016-10-31 08:13:55 -0700

    added code

--- Registration/readData.js
+++ Registration/readData.js
@@ -3,6 +3,11 @@
         var ctrls = {};
         ctrls.firstName = document.getElementById("txtFirstName");
         ctrls.lastName = document.getElementById("txtLastName");
+
+        ctrls.countries = document.getElementById("ddlCountry");
+
+        ctrls.agreeTerms = document.getElementById("chkAgree");
+
         ctrls.registerButton = document.getElementById("btnRegister");
 
         function readData() {
@@ -10,6 +15,10 @@
             var profile = {};
             profile.firstName = ctrls.firstName.value;
             profile.lastName = ctrls.lastName.value;
+            profile.country = ctrls.countries.value;
+            profile.agreeTerms = ctrls.agreeTerms.checked;
+
+            var indexData = ctrls.countries.selectedIndex;
 
         }
 
@@ -31,7 +40,24 @@
 
         }
 
+
+        function enableRegister(evt) {
+            //console.log(evt);
+            console.log(this.checked);
+            if (this.checked) {
+                //to show the button.
+                ctrls.registerButton.style.display ="block";
+            } else {
+                
+                //to hide the button
+                ctrls.registerButton.style.display = "None";
+            }
+
+        }
+
         function bindEvents() {
+            ctrls.agreeTerms.addEventListener("click", enableRegister);
+
             ctrls.registerButton.addEventListener("click", readData);
             ctrls.registerButton.addEventListener("mouseover", changeColor);
             ctrls.firstName.addEventListener("keypress", alphabetsOnly);
