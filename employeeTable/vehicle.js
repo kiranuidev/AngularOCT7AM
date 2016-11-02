3400709c681bc51a21f808d1ccf37d505eb0cc51
narasimha kbnd <narasimha kbnd>
2016-11-02 08:06:52 -0700

    added

---
+++ employeeTable/vehicle.js
@@ -0,0 +1,26 @@
+
+
+(function () {
+    window.name="kiran";
+    //object literals.
+     var vehicle ={};
+     vehicle.engine="";
+     vehicle.type="";
+     vehicle.body="";
+     vehicle.tyres="";
+   /* function vehicle() {
+        this.tyres = 0;
+        this.engine = "";
+        this.color="";
+        var vin="";
+    }
+*/
+    var bike = new vehicle();
+  
+    var car = new vehicle();
+   
+    console.log(bike);
+    console.log(car);
+
+
+})();
